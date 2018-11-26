d1d19be7eddb1dc56dfa960065f1b907f02d95ab
L4M0S <13170563+L4M0S@users.noreply.github.com>
2018-11-26 16:48:02 -0600

    Login

--- js/login.js
+++ js/login.js
@@ -1,31 +1,9 @@
-var insertar=document.getElementById("usuario");
-innerHTML = "rodolfo";
-
 function redireccionar(){
 	
 } 
 
 function iniciar()
 {
-	var usuario=document.getElementById('campousuario').value;
-	var pass=document.getElementById('campopass').value;
-
-	
-	if (usuario=="rodolfo" && pass=="secreto") {
-		document.location.href="index.html";
-	}
-	else
-	{
-		alert("Datos no validos");
-	}
-}
-
-function crear()
-{
-	var usuario=document.getElementById('campousuario').value;
-	var pass=document.getElementById('campopass').value;
-
-	
 	if (usuario=="rodolfo" && pass=="secreto") {
 
 		document.location.href="indexr.html";
